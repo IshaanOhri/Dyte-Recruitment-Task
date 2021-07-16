b3635d4e2b603fce071e7f14f3684fbadfa2544e
Ishaan Ohri <ishaan99ohri@gmail.com>
2021-07-16 16:07:23 +0530

    Added basic env variables and app.ts

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+/*
+ * @Author: Ishaan Ohri
+ * @Date: 2021-07-16 15:56:57
+ * @Last Modified by: Ishaan Ohri
+ * @Last Modified time: 2021-07-16 16:06:50
+ * @Description: Driver file of the server
+ */
+
+import cors from "cors";
+import express, { Application } from "express";
+import { PORT, HOST } from "./config/env";
+import logger from "./log/config";
+
+// Initializing Express App
+const app: Application = express();
+
+// CORS
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+// Connect to Database
+require('./database/database');
+
+// Import routers
+app.use('/api/v1', router);
+
+// Not found handler
+app.use(notFound);
+
+// All response handlers
+app.use(responseHandler);
+
+// Start Express App
+app.listen(PORT, HOST, () => {
+	logger.info(`🚀 Server running on http://${HOST}:${PORT} 🚀`);
+});
--- src/config/env.ts
+++ src/config/env.ts
@@ -2,6 +2,10 @@
  * @Author: Ishaan Ohri
  * @Date: 2021-07-16 15:50:37
  * @Last Modified by: Ishaan Ohri
- * @Last Modified time: 2021-07-16 15:51:03
+ * @Last Modified time: 2021-07-16 16:06:37
  * @Description: Collective export of all environment variables
  */
+
+export const NODE_ENV: string = String(process.env.NODE_ENV);
+export const PORT: number = Number(process.env.PORT);
+export const HOST: string = String(process.env.HOST);
