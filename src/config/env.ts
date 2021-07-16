4243ad4dd6637221e84e0a1b3ac300fc9c03c501
Ishaan Ohri <ishaan99ohri@gmail.com>
2021-07-16 15:52:06 +0530

    Added config folder structure and response codes

---
+++ src/config/env.ts
@@ -0,0 +1,7 @@
+/*
+ * @Author: Ishaan Ohri
+ * @Date: 2021-07-16 15:50:37
+ * @Last Modified by: Ishaan Ohri
+ * @Last Modified time: 2021-07-16 15:51:03
+ * @Description: Collective export of all environment variables
+ */
---
+++ src/database/database.ts
@@ -0,0 +1,7 @@
+/*
+ * @Author: Ishaan Ohri
+ * @Date: 2021-07-16 15:48:16
+ * @Last Modified by: Ishaan Ohri
+ * @Last Modified time: 2021-07-16 15:48:38
+ * @Description: File for the connection to MongoDB Database
+ */
